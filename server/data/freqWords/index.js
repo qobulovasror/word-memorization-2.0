a71e05df47381562f4e08a7eb357e7ae4a68df9f
qobulovasror <qobulovasror0@gmail.com>
2023-12-12 19:22:30 +0500

    fix sever: game one

--- server/data/freqWords/index.js
+++ server/data/freqWords/index.js
@@ -1,7 +1,7 @@
-import a1 from './a1.json'
-import a2 from './a2.json'
-import b1 from './b1.json'
-import b2 from './b2.json'
-import c1 from './c1.json'
+import a1 from './a1.json' assert { type: "json" };
+import a2 from './a2.json' assert { type: "json" };
+import b1 from './b1.json' assert { type: "json" };
+import b2 from './b2.json' assert { type: "json" };
+import c1 from "./c1.json" assert { type: "json" };
 
 export {a1, a2, b1, b2, c1}
